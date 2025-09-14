16b56b5d49121820382de07b0fdd44fa49974818
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-14 15:35:15 +0530

    feat: add a shared constants file to be shared between backend and frontend

---
+++ src/shared/constants.js
@@ -0,0 +1,12 @@
+/**
+ * Stack Overflow Clone
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/shared/constants.js
+ * License: MIT (see LICENSE)
+*/
+
+export const VERSION = "0.1.0-dev";
+
+export const API_BASE_URL = "/api/";
+export const QUESTION_API_URL = `${API_BASE_URL}questions/`;
+
