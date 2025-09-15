f33d103827551dd606d22ef88e732b513abcd3fb
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-15 08:32:57 +0530

    feat: add signup function in user controller

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -7,4 +7,18 @@
 
 import User from "../models/User.js";
 
-export const signup = (req, res) => {};
+export const signup = async (req, res) => {
+    const { username, name } = req.body;
+
+    if (!username || !name) {
+        return res.status(400).json({ message: "Username and name are required" });
+    }
+
+    const exists = await User.findOne({ username });
+    if (exists) {
+        return res.status(409).json({ message: "Username already exists" });
+    }
+
+    const user = await User.create({ username, name });
+    res.status(201).json({ ...user._doc });
+};
