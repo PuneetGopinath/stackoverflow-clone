c3cbdd0bf0ebee730471b5181cc3aa3cd46db2db
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-15 08:37:13 +0530

    feat: update Home component to highlight commenting feature on Questions

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -5,13 +5,17 @@
  * License: MIT (see LICENSE)
 */
 
+import { Link } from "react-router";
+
 export default function Home() {
     return (
         <>
             <h1>Stack Overflow Clone</h1>
             <p>A web app similar to stack overflow that can be used for question and answers mainly for programming and software engineering related.</p>
 
-            <p><bold>Website is in construction</bold></p>
+            <p><bold>So far, we have implemented the feature of commenting on Questions</bold></p>
+            <p>Click on the question below to view and add comments</p>
+            <Link to="/questions/92b1a90f-f4ed-4dea-a873-7dd599a3ce2e">Question 1</Link>
         </>
     );
 };
